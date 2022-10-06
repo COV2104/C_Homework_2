3160414c15c2be24c78c5fd2e14ba30ac352e313
COV <tsehlo@mail.ru>
2022-10-06 12:23:30 +0300

    Добавили обработку исключений

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -2,8 +2,30 @@
 // на выходе показывает вторую цифру этого числа.
 // 456 -> 5
 
-Console.WriteLine("Введите трехзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int num2 = num / 10;
-Console.WriteLine($"Вторая цифра в числе = {num2 % 10}");
-
+string SecondDigitNum()
+{
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    string result = "";
+    int num1 = Math.Abs(num);
+    int num3 = 0;
+    if (num1 >= 10)
+    {
+        while (num1 >= 10)
+        {
+            num3 = num1 % 10;
+            num1 = num1 / 10;
+        }
+        result = $"Вторая цифра в числе = {num3}";
+    }
+    else result = "Второй цифры в числе нет ";
+    return result;
+}
+try
+{
+    Console.WriteLine(SecondDigitNum());
+}
+catch
+{
+    Console.WriteLine("error: Необходимо вводить цифры");
+}
